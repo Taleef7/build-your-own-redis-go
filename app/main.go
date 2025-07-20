fedf3ffa50151da7a8285f714fbab2c0656311c9
Taleef7 <24100108@lums.edu.pk>
2025-07-20 00:51:37 -0400

    Stage5

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"bufio"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -27,4 +29,33 @@ func main() {
 		os.Exit(1)
 	}
 	conn.Write([]byte("+PONG\r\n"))
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if strings.TrimSpace(text) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		}
+		//conn.Write([]byte("-Error invalid command: '" + text + "'\r\n"))
+	}
 }
